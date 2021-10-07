783de44fe20aa8aafe7239ce825ef7c1047b357c
Thinkman Wang <wangxf1985@gmail.com>
2021-10-07 11:42:08 +0800

    update

--- mysql_redis/back_redis.sh
+++ mysql_redis/back_redis.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 #redis 源ip
-src_ip=6.0.170.7
+src_ip=172.16.0.2
 #redis 源port
 src_port=6379
+src_password=Ab123145
+src_db=0
 
 #redis 目的ip
-dest_ip=6.0.170.15
+dest_ip=172.16.0.2
 #redis 目的port
 dest_port=6379
+dest_password=Ab123145
+dest_db=1
 
 #要迁移的key前缀
 key_prefix=
 
 i=1
 
-redis-cli -h $src_ip -p $src_port  keys "${key_prefix}*" | while read key
+redis-cli -u redis://$src_password@$src_ip:$src_port/$src_db keys "${key_prefix}*" | while read key
 do
-  redis-cli -h $dest_ip -p $dest_port  del $key
-  redis-cli -h $src_ip -p $src_port  --raw dump $key | perl -pe 'chomp if eof' | redis-cli -h $dest_ip -p $dest_port  -x restore $key 0
+  redis-cli -u redis://$dest_password@$dest_ip:$dest_port/$dest_db del $key
+  redis-cli -u redis://$src_password@$src_ip:$src_port/$src_db --raw dump $key | perl -pe 'chomp if eof' | redis-cli -u redis://$dest_password@$dest_ip:$dest_port/$dest_db -x restore $key 0
   echo "$i migrate key $key"
   ((i++))
 done
